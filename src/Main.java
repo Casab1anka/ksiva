44076095678a8dbb879ec0424c4e9def7fa6a82e
Casab1anka <Pipito2021>
2021-12-01 19:25:58 +0300

    Initial commit2121

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(){
+        Person a = new Person(13,"Casab1anka");
+        String result = a.toString();
+        System.out.println(result);
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+    int age;
+    String name;
+    Person (int age, String name){
+        this.age = age;
+        this.name = name;
+    }
+    @Override
+    public String toString(){
+       return age + name;
+    }
+}
